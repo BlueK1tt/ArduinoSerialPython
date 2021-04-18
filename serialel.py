7177bc4bbd14c34f016b29a545ad9f5210221256
BlueThinks <25419801+BlueK1tt@users.noreply.github.com>
2021-04-19 00:34:15 +0300

    Changed logic
    Changed the way "final" string/data is created to be more "readable"

--- serialel.py
+++ serialel.py
@@ -6,7 +6,8 @@ cels = '°C'
 oldtemp = '0'
 oldtime = 0
 i = 0
-time.sleep(1);
+br = "\n"
+time.sleep(1.5);
 
 #attributes for serial
 ser = serial.Serial()
@@ -45,23 +46,27 @@ while ser.is_open == True:
         h,m,s = times.split(":")
         currsecs = int(datetime.timedelta(hours=int(h),minutes=int(m),seconds=int(s)).total_seconds())
         timediff = currsecs - oldtime
-        str1 = '+' + str(timediff) +'s '
+        str1 = 'Time:' + '+' + str(timediff) +'s'
         print(str1)
         
         #when if, count the time difference between times and put +smth
         str2 = temp[2:7] #cut the input data from 2 to 7 position of letters, could have problem if temperature goes over 100 or under 0
         print(str2)
+
+        dataTime = str1
+        dataTemp = "Temp:" + str2 
+
         str3 = "{"
         str4 = "}"
-        str5 = str3 + str1 + ", " + str2 + str4
-        str6 = '"' + str5 + '"'
-        vals = str6
+        str5 = str3 + dataTime + ", " + dataTemp + str4
+        strF = '"' + str5 + '"'
+        vals = strF
         data = json.loads(vals)
 
         with open('./temps.json', 'a') as outfile:
             if (i == 0):
                 outfile.write(times)
-                outfile.write(', \n');
+                outfile.write(': \n');
             json.dump(data, outfile, indent= 2 )
             outfile.write(', \n')
             i += 1
@@ -72,4 +77,3 @@ while ser.is_open == True:
     oldtemp = temp
 else:
     time.sleep(1)
-
