86f313c152959eb54de898d7e261fa8b07fed3d4
BlueThinks <25419801+BlueK1tt@users.noreply.github.com>
2021-04-23 15:49:54 +0300

    Comments
    Added bunch of "clearing" comments for better understanding of code

--- serialel.py
+++ serialel.py
@@ -1,7 +1,7 @@
 import datetime
 import time
 import serial #need to download pyserial
-import json
+import json #get the JSON library for JSON data "output"
 cels = '°C'
 oldtemp = '0'
 oldtime = 0
@@ -37,9 +37,9 @@ while ser.is_open == True:
     serTemp = ser.readlines(1)
     temp = ''.join(str(e) for e in serTemp) 
     time.sleep(1)
-    ser.flushInput() 
+    ser.flushInput() #needed to literally flush possible serial buffered data to be on time
 
-    if(oldtemp != temp):
+    if(oldtemp != temp): #just cheks if the incoming data is "new" in compared to last added data dump
         
         times = time.strftime('%H:%M:%S')
         #convert time to seconds
@@ -47,12 +47,13 @@ while ser.is_open == True:
         currsecs = int(datetime.timedelta(hours=int(h),minutes=int(m),seconds=int(s)).total_seconds())
         timediff = currsecs - oldtime
         str1 = 'Time:' + '+' + str(timediff) +'s'
-        print(str1)
+        print(str1) #print how much time has passed since last dump
         
         #when if, count the time difference between times and put +smth
         str2 = temp[2:7] #cut the input data from 2 to 7 position of letters, could have problem if temperature goes over 100 or under 0
         print(str2)
-
+        
+        #just bunch of string theory, this all is for the sake of creating the new data into JSON file look nice
         dataTime = str1
         dataTemp = "Temp:" + str2 
 
@@ -63,17 +64,17 @@ while ser.is_open == True:
         vals = strF
         data = json.loads(vals)
 
-        with open('./temps.json', 'a') as outfile:
+        with open('./temps.json', 'a') as outfile: #open "session" for JSON file writing
             if (i == 0):
-                outfile.write(times)
-                outfile.write(': \n');
-            json.dump(data, outfile, indent= 2 )
+                outfile.write(times) #add current time
+                outfile.write(': \n'); #next row
+            json.dump(data, outfile, indent= 2 ) #dump the whole string with +time and temperature
             outfile.write(', \n')
-            i += 1
-            print(i);
-        oldtime = currsecs
+            i += 1 #i for monitoring how many times temperature has been written
+            print(i); #shows only in console, since it would take more space to write it into the JSON
+        oldtime = currsecs #change the value of time, this is needed in order to calculate time difference between temperature cheks
     else:
         None
-    oldtemp = temp
-else:
-    time.sleep(0.5)
+    oldtemp = temp #replace temprature data value, this is needed in order to check if same temperature was previusly added
+else:              #if the temperature has not changed in the matter of time lasted, will not write new line into JSON
+    time.sleep(0.5)#will save more space in the file, if nothing has changed, sleep 0.5s. really not needed, but gives treshold to new data 
